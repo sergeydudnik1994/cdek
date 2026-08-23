fa4841dc3d96e04829d19ee774cd88a176efb2a1
Dudidududududidudidudidu <sergeydudnik1994@gmail.com>
2026-08-23 14:30:26 +0300

    Add CORS headers and handle preflight requests

--- cdek-service.php
+++ cdek-service.php
@@ -1,5 +1,14 @@
 <?php
-header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Content-Type: application/json; charset=utf-8');
+
+// Обрабатываем preflight OPTIONS-запрос браузера
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
